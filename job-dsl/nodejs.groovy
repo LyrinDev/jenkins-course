64f9f5f3a0ade02de7c09dd619990189f7a5459c
LyrinDev <111134183+LyrinDev@users.noreply.github.com>
2022-08-26 11:19:43 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('arcanalea')
+            node / gitConfigEmail('melliott2003@hotmail.com')
         }
     }
     triggers {
